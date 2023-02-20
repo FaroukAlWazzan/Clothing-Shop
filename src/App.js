81c77451b229c6729bac2d3f7fbf1cc7617dbed4
Alwazzan <a7laaroka2013@gmail.com>
2023-02-20 21:26:58 +0300

    explaining the star operator

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
+        {/* the star for including all sub routes */}
         <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
